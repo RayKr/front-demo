37219d933d66b0a8ec3066caa5ff6c77b5534cb1
raykr <ray_ke@163.com>
2017-02-14 22:24:22 +0800

    webpack-demo

---
+++ webpack-demo/app/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JingZL on 2017/2/14.
+ */
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], '');
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by JingZL on 2017/2/14.
+ */
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: './dist'
+    }
+};
